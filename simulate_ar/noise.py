Fixing some issues related to units/noise

simulate_ar/noise.py
@@ -27,7 +27,7 @@ class TsysNoise:
             T = self.tsys + self.tsky * (subint.ssb_freq/self.tsky_ref_freq)**self.tsky_index
             chanbw= simulated_obs.obs_setup.bw/simulated_obs.obs_setup.nchan
             tobs = subint.tsub
-            sefd = T/(self.gain*np.sqrt(2*chanbw*tobs))
+            sefd = T/(self.gain*np.sqrt(2*chanbw*1e6*(tobs/simulated_obs.obs_setup.nbin)))
             for ichan in range(simulated_obs.obs_setup.nchan):
                 for ipol in range(simulated_obs.obs_setup.npol):
                     subint.data[ipol,ichan] += np.random.normal(scale=sefd[ichan], size=subint.nbin)

simulate_ar/profile.py
@@ -29,12 +29,14 @@ class VonMisesProfile:
 
 
     def compute(self,phase, freq, epoch,nbin):
+        # print(f"phase {phase}")
+        # print(f"freq {freq}")
         freq = np.atleast_1d(freq)
         outprof = np.zeros((len(freq), nbin))
         phase_axis = np.arange(nbin)/nbin
         for vm in self.components:
             amp = vm.amp * (vm.ref_freq/freq)**vm.spectral_index
-            outprof += np.outer(amp, np.exp(((np.cos(2*np.pi*(phase_axis - phase+vm.phase))-1) *vm.k)))
+            outprof += np.outer(amp, np.exp(((np.cos(2*np.pi*(phase_axis - phase-vm.phase))-1) *vm.k)))
 
         return outprof
 

simulate_ar/simulate.py
@@ -337,7 +337,7 @@ class Simulation:
 
         for isub,si in enumerate(self.subints):
             
-            prof = si.data
+            prof = si.data*1e3 # PSRFITS data is in mJy, data are in Jy.
 
             profmax = np.reshape(np.amax(prof,axis=2),(npol,nchan,1))
             profmin = np.reshape(np.amin(prof,axis=2),(npol,nchan,1))
@@ -387,7 +387,7 @@ class Simulation:
             bary_epoch = subint.ssb_epoch
             phase0 = self.ssb_predictor.getPrecisePhase(bary_epoch.imjd, np.longdouble(bary_epoch.fmjd), 1e12)
             prof = generator.compute(phase0, subint.ssb_freq, bary_epoch, subint.nbin)
-            print("phase",phase0)
+            #print("phase",phase0)
             if len(prof.shape) == 2:
                 prof = np.reshape(prof,(1,self.obs_setup.nchan,subint.nbin))
             if prof.shape != subint.data.shape:
@@ -396,6 +396,7 @@ class Simulation:
             dm_delay = propagation_model.get_delays(subint.ssb_freq) # Maybe more parameters?
             for ichan in range(self.obs_setup.nchan):
                 phase = self.ssb_predictor.getPrecisePhase(bary_epoch.imjd,np.longdouble(bary_epoch.fmjd-dm_delay[ichan]/86400.0), 1e12)
+                #print("ichan,phase=",ichan,phase)
                 for ipol in range(self.obs_setup.npol):
                     subint.data[ipol,ichan] = rotate_phs_1d(prof[ipol,ichan],phase-phase0)
 